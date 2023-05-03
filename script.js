20dccffb5aa86ba2c0d4772232b0d82dd0d8d996
Султан <carameleclipsimus@gmail.com>
2023-05-03 09:59:41 +0600

    feat: second task of destructuring is done

--- script.js
+++ script.js
@@ -14,4 +14,50 @@ function myFunc(...arg) {
   return { first, other };
 }
 const result = myFunc("a", "b", "c", "d");
-console.log(result);
+// console.log(result);
+
+// second
+
+/*
+Организовать функцию getInfo, которая принимает объект вида
+{ name: ..., info: { employees: [...], partners: [ … ] } }
+и выводит в консоль имя (если имени нет, показывать ‘Unknown’) и первые две компании из массива partners:
+
+const organisation = {
+
+name: 'Google',
+
+info: { employees: [‘Vlad’, ‘Olga’], partners: ['Microsoft', 'Facebook', 'Xing'] }
+
+};
+
+
+
+getInfo(organisation); →
+
+Name: Google
+
+Partners: Microsoft Facebook
+*/
+const organisation = {
+  name: "Google",
+
+  info: {
+    employees: ["Vlad", "Olga"],
+    partners: ["Microsoft", "Facebook", "Xing"],
+  },
+};
+
+const getInfo = ({ ...org }) => {
+  // console.log(org);
+  const {
+    name,
+    info: {
+      partners: [first, second, ...others],
+    },
+  } = org;
+  return `Name: ${name}, Parters: ${first}, ${second};`;
+};
+
+const result2 = getInfo(organisation);
+console.log(result2);
